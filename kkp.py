14f1e4548ef083e6643a4e3ea6bcf50849954134
xxxc137 <77255211+Xwens-e@users.noreply.github.com>
2023-07-23 13:18:47 +0800

    Update kkp.py

--- kkp.py
+++ kkp.py
@@ -1,14 +1,14 @@
-#23/7/19
+#23/7/23
 #@xxxc137
+#变量 kkpck 抓UID;SESSIONID 多号换行隔开
 import requests
 import os
 
-# 定义多个账号的cookie值，使用换行进行分隔
 cookies = os.environ.get("kkpck").split("\n")
+signin_url = "http://newapi.kukupao.top/tuhao/signin?"
+lottery_url = "https://m.kukupao.com.cn/member/getblindboxaward"
 
-url = "http://newapi.kukupao.top/tuhao/signin?phonetype=Mi+10&phonebrand=Xiaomi&osversion=12&osversioncode=31&version=11.7.10&versioncode=129&channel=%E5%AE%98%E7%BD%91&channelid=1&appid=1&sw=1080&sh=2339&dt=%E6%89%8B%E6%9C%BA&network=2014&carrier=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8&oaid=2ac4ea344ca1d124&um_oaid=2ac4ea344ca1d124&miui=1&skin=white&cryptdata=nobiE%2BtIzCBS%2FymcImybXbLUvoqF9x%2BsouvTU0eo2FZdJ9EhD%2F2YsmYdtCHg9fRu5xNzhyS5orjZYWdnsSotbW3y%2F%2BUqiAmAqmiOJfH4brSX%2BUM4bbA07EQn2MpvdbsyY1tXO8cfNAC9DG503cifkuDCgETseVyII%2FgtgNAAG3k%3D"
-
-print("///////////////////签到开始//////////////////")
+print("///////////////////开始签到//////////////////")
 
 for cookie in cookies:
     headers = {
@@ -22,16 +22,25 @@ for cookie in cookies:
         "Cookie": cookie
     }
 
-    response = requests.post(url=url, headers=headers)
+    response = requests.post(url=signin_url, headers=headers)
     if response.ok:
         data = response.json()
         msg = data.get("msg")
 
         if msg == "\u4eca\u65e5\u5df2\u7b7e\u5230":
-            print("🎈已经签到过了")
+            print("꯭🎈꯭已꯭经꯭签꯭到꯭过꯭了꯭")
         elif msg == "":
-            print("🎉签到成功")
+            print("꯭🎉꯭签꯭到꯭成꯭功꯭")
         elif msg == "\u7528\u6237\u9a8c\u8bc1\u5931\u8d25\uff0c\u8bf7\u91cd\u65b0\u767b\u9646":
-            print("❌cookie已过期")
+            print("꯭❌꯭c꯭o꯭o꯭k꯭i꯭e꯭已꯭过꯭期꯭")
     else:
         print("请求失败")
+
+    print("///////////////////开始抽奖//////////////////")
+    
+    lottery_response = requests.get(url=lottery_url, headers=headers)
+    if lottery_response.ok:
+        lottery_result = lottery_response.content.decode('unicode_escape').replace('\\/', '/')
+        print("抽奖结果:", lottery_result)
+    else:
+        print("抽奖请求失败")
