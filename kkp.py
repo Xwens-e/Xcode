f80facb5db27f4e0149f03e12a6d5399294e4eef
xxxc137 <77255211+Xwens-e@users.noreply.github.com>
2023-07-24 20:09:56 +0800

    Update kkp.py

--- kkp.py
+++ kkp.py
@@ -1,4 +1,4 @@
-#23/7/23
+#23/7/24 增加分享 默认分享10次
 #@xxxc137
 #变量 kkpck 抓UID;SESSIONID 多号换行隔开
 import requests
@@ -7,17 +7,12 @@ import os
 cookies = os.environ.get("kkpck").split("\n")
 signin_url = "http://newapi.kukupao.top/tuhao/signin?"
 lottery_url = "https://m.kukupao.com.cn/member/getblindboxaward"
+extra_url = "http://newapi.kukupao.top/member/donesharetask?phonetype=Mi+10&phonebrand=Xiaomi&osversion=12&osversioncode=31&version=11.7.10&versioncode=129&channel=%E5%AE%98%E7%BD%91&channelid=1&appid=1&sw=1080&sh=2339&dt=%E6%89%8B%E6%9C%BA&network=2014&carrier=%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8&oaid=2ac4ea344ca1d124&um_oaid=2ac4ea344ca1d124&miui=1&skin=night&cryptdata=EbHblWaZGgZD0U6nwYezj9V3zzvU2O3SG3cKZNcxKEOwrl%2BwFrxlq8JN0w1%2FIte%2BM%2FrMNvBO9w5sOAVYENvB29PoEM%2BlnPauVAo02OD3%2BK0je9G%2FMn%2BNkQSv9xxzTs5OevGM1x4lqxreRFFVlGuxEcOR9EZh7WM6SNOcBgsEv7c%3D"  # Additional request URL
 
 print("///////////////////开始签到//////////////////")
 
 for cookie in cookies:
     headers = {
-        "Connection": 'keep-alive',
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Cache-Control": 'no-cache, must-revalidate, max-age=0',
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; Mi 10 Build/SKQ1.211006.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.58 Mobile Safari/537.36",
-        "accept": "*/*",
-        "connection": "Keep-Alive",
         "Accept-Encoding": "gzip,deflate",
         "Cookie": cookie
     }
@@ -44,3 +39,13 @@ for cookie in cookies:
         print("抽奖结果:", lottery_result)
     else:
         print("抽奖请求失败")
+
+    print("///////////////////开始分享//////////////////")
+
+    for i in range(10):
+        extra_response = requests.get(url=extra_url, headers=headers)
+        if extra_response.ok:
+            extra_result = extra_response.content.decode('unicode_escape').replace('\\/', '/')
+            print("分享", i+1, ":", extra_result)
+        else:
+            print("分享", i+1, "失败")
