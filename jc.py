86a1b5ad9fe03bf236e075eb839a637a24d9d02f
xxxc137 <77255211+Xwens-e@users.noreply.github.com>
2023-07-25 16:56:38 +0800

    Update jc.py

--- jc.py
+++ jc.py
@@ -20,19 +20,23 @@ if not cookies_str:
 
 cookies_list = cookies_str.split("\n")
 
-for index, cookies in enumerate(cookies_list):
-    headers["Cookie"] = cookies
+result_list = []  # 存储签到结果的列表
 
 print("///////////////////签到开始//////////////////")
 
-response = requests.post(url=url, headers=headers)
-data = response.json()
-ret = data.get('ret')
+for index, cookies in enumerate(cookies_list):
+    headers["Cookie"] = cookies
 
-if ret == 1:
-    msg = data.get('msg')
-    # 提取 msg 中的数字
-    flow = ''.join(filter(str.isdigit, msg))
-    print("🎉签到成功，获得 {} 流量".format(flow))
-elif ret == 0:
-    print("🍻今日已签到")
+    response = requests.post(url=url, headers=headers)
+    data = response.json()
+    ret = data.get('ret')
+
+    if ret == 1:
+        msg = data.get('msg')
+        # 提取 msg 中的数字
+        flow = ''.join(filter(str.isdigit, msg))
+        result = f"账号{index + 1} 签到成功，获得 {flow} 流量"
+        print("🎉", result)
+        result_list.append(result)
+    elif ret == 0:
+        print("🍻账号{} 今日已签到".format(index + 1))
